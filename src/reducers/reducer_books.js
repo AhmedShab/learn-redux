44b593ee61fe99b2f1246a29320d1611c1c53cdc
AhmedShab <ahmed.vuw@gmail.com>
2016-06-27 09:50:48 +1200

    finishing off

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function () {
   return [
-    {title: 'Javascript, the good parts'},
-    {title: 'Harry potter'},
-    {title: 'final fatasy'},
-    {title: 'kingdom hears'}
+    {title: 'Javascript, the good parts', pages: 101},
+    {title: 'Harry potter', pages: 39},
+    {title: 'final fatasy', pages: 59},
+    {title: 'kingdom hears', pages: 1}
   ];
 }
